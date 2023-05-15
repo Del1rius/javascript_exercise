21c0b42d47efabdd0e97545f94cdb0c1e92d8cb1
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-15 15:00:52 +0200

    Oslins exercise complete

--- js/index.js
+++ js/index.js
@@ -6,3 +6,19 @@ alert('This is the browsers alert box');
 var name = 'John'
 
 alert(name);
+
+var interestRate = 0.3;
+var interestRate = 1;
+
+console.log(interestRate);
+
+
+const interestRate = 0.3;
+
+console.log(interestRate);
+
+const Person = {name: 'timothy', surname: 'barry', age: 12, occupation: 'idiot', gender: 'Ask jesus'};
+let {name, surname, age, occupation, gender} = Person;
+console.log(name , surname, age, occupation, gender);
+
+
