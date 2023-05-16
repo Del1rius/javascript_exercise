e7bc8f2ab2d450b2cc1393085725d1bb093f8759
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-16 11:20:19 +0200

    more js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,66 @@
 // console log
 console.log("Hello World");
 
-alert('This is the browsers alert box');
+alert("This says you must get a life");
 
-var name = 'John'
+// var name = 'John'
 
-alert(name);
+// alert(name);
 
-var interestRate = 0.3;
-var interestRate = 1;
+// var interestRate = 0.3;
+// var interestRate = 1;
 
-console.log(interestRate);
-
+// console.log(interestRate);
 
 const interestRate = 0.3;
 
 console.log(interestRate);
 
-const Person = {name: 'timothy', surname: 'barry', age: 12, occupation: 'idiot', gender: 'Ask jesus'};
-let {name, surname, age, occupation, gender} = Person;
-console.log(name , surname, age, occupation, gender);
+const Person = {
+  name: "timothy",
+  surname: "barry",
+  agee: 12,
+  occupation: "idiot",
+  gender: "Ask jesus",
+};
+let { name, surname, agee, occupation, gender } = Person;
+console.log(name);
+console.log(surname);
+console.log(agee);
+console.log(occupation);
+console.log(gender);
 
+document.write("Hello world ");
+
+document.querySelector("#greeting").innerText = "Well Done";
+document.write("<p>A Text</p>");
+document.write("<p>New content</p>");
+
+function myFunction() {
+  alert("You are a poes");
+}
+
+let string = "a random string";
+console.log(string);
+
+let a, b, c;
+a = b = c = 0;
+c--;
+document.write("The value of a is: ", a + "<br>");
+document.write("The value of b is: ", b + "<br>");
+document.write("The value of c is: ", c + "<br>");
+
+let namee = "John";
+let paragraph = "this is john's car";
+let quotation = 'John said, "This is my car"';
+
+let str = "Hello There";
+let str2 = 'Single quotes work just fine';
+let phrase = 'can embed ${str}';
+
+let age = 30.1; //This is known as a literal
+let num = 2; // This is an integer
+let num2 = 1.3; 
+let num3 = Infinity;
+let num4 = 'something here too'/2;
 
